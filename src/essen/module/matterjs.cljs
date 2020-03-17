e0f1eda3f5b603bb12996ad5fc6d3ae42873e5db
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-03-17 19:55:36 +0100

    Add collision events

--- src/essen/module/matterjs.cljs
+++ src/essen/module/matterjs.cljs
@@ -1,6 +1,6 @@
 (ns essen.module.matterjs
   (:require
-   ["matter-js" :as MatterJs :refer [Engine Render Bodies World Mouse MouseConstraint Composite]]
+   ["matter-js" :as Matter :refer [Engine Render Bodies World Mouse MouseConstraint Composite]]
    [integrant.core :as ig]))
 
 ;; Remove a body
@@ -28,21 +28,35 @@
     (reset! points (js->clj (engine->points @engine1)))))
 
 (defmethod ig/init-key :matterjs/start [_ opts]
-  (println "Creating engineeeee")
   (let [canvas (.getElementById js/document "game")
         engine (.create Engine (clj->js {:render {:canvas canvas
                                                   :width (.-width canvas)
                                                   :height (.-height canvas)}}))
         boxA (.rectangle Bodies 400 200 20 31 #js {:restitution 1})
-        circleA (.circle Bodies 400 200 100 #js {:restitution 0.4})
+        circleA (.circle Bodies 400 200 100 #js {:restitution 0.4 :label "Some Circle"})
         ground (.rectangle Bodies 400 610 810 60 (clj->js {:isStatic true
                                                            :restitution 1}))
         mouseConstraint (.create MouseConstraint engine
                                  (js->clj {:mouse (.create Mouse canvas)}))]
-    (.add World (.-world engine) ground)
     (.add World (.-world engine) boxA)
     (.add World (.-world engine) circleA)
+    (.add World (.-world engine) ground)
     (.add World (.-world engine) mouseConstraint)
+
+    (.on Matter/Events engine "collisionStart"
+         (fn [event]
+           (let [bodyA (.-bodyA (aget (.-pairs event) 0))
+                 bodyB (.-bodyB (aget (.-pairs event) 0))]
+             (println (.-label bodyA))
+             (.setVelocity Matter/Body bodyA #js{:x 0 :y -10}))))
+
+    ;; constant velocity
+    ;; (set! (.-restitution circleA) 1)
+    ;; (set! (.-friction circleA) 0)
+    ;; (set! (.-frictionAir circleA) 0)
+    ;; (set! (.. engine -world -gravity -y) 0)
+    ;; (set! (.. engine -world -gravity -x) 0)
+
     (reset! box boxA)
     (reset! engine1 engine)
     run))

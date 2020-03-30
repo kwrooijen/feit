426311ba48b3948f70a26a8c61f75ba9ccae32e9
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-03-30 20:20:31 +0200

    Remove cardo

--- src/cardo/component/equipment.cljs
+++
@@ -1,9 +0,0 @@
-(ns cardo.component.equipment
-  (:require
-   [integrant.core :as ig]))
-
-(defmethod ig/init-key :component/equipment [_ _opts]
-  {:equipment/armor :cool})
-
-(def config
-  {[:essen/component :component/equipment] {}})
--- src/cardo/middleware/stats.cljs
+++
@@ -1,9 +0,0 @@
-(ns cardo.middleware.stats
-  (:require
-   [integrant.core :as ig]))
-
-(defmethod ig/init-key :middleware.stats/invincible [_ _opts]
-  (fn [_context event _state]
-    (assoc event
-           :event/damage 0
-           :event/invincible? true)))

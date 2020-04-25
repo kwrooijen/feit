579deec553fcb8aecd098160ba6174f3124c6627
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-04-25 13:13:24 +0200

    Add pivot-x / y for rectangles

--- src/rooij/module/matterjs/interface/rectangle.cljs
+++ src/rooij/module/matterjs/interface/rectangle.cljs
@@ -15,13 +15,14 @@
     [(.-x velocity)
      (.-y velocity)]))
 
-(defrecord MatterPhysics2DRectangle [body x y w h]
+(defrecord MatterPhysics2DRectangle [body x y w h pivot-x pivot-y]
   RooijPhysics2DRectangle)
 
 (extend-protocol RooijGeneral2DPosition
   MatterPhysics2DRectangle
-  (set-position [{:keys [body] :as this} x y angle]
-    (.setPosition Body body #js {:x x :y y})
+  (set-position [{:keys [body pivot-x pivot-y] :as this} x y angle]
+    (.setPosition Body body #js {:x (+ x pivot-x)
+                                 :y (+ y pivot-y)})
     (.setAngle Body body angle)
     (assoc this :x x :y y))
   (get-position [{:keys [body w h] :as this}]
@@ -56,17 +57,26 @@
   (-pr-writer [this writer _]
     (write-all writer (str "MatterPhysics2DRectangle#" (into {} (assoc this :body (symbol "#MatterJs/Body")))))))
 
-(defn- opts->rectangle [{:keys [x y w h static]}]
-  (.rectangle Bodies x y w h #js {:isStatic static}))
+(defn- opts->rectangle [{:keys [x y w h static? sensor?]
+                         pivot-x :pivot/x
+                         pivot-y :pivot/y}]
+  (.rectangle Bodies (+ x pivot-x) (+ y pivot-y) w h
+              #js {:isStatic static? :isSensor sensor?}))
 
-(defn- new-rectangle [k {:context/keys [scene-key] :keys [x y w h] :as opts}]
+(defn- new-rectangle [k {:context/keys [scene-key entity-key]
+                         :keys [x y w h]
+                         pivot-x :pivot/x
+                         pivot-y :pivot/y
+                         :as opts}]
   (let [rectangle (opts->rectangle opts)]
     (when (false? (:auto-rotate opts))
       (.setInertia Body rectangle js/Infinity))
     (.add World (state/get-world scene-key) rectangle)
     (set! (.-component-key rectangle) k)
+    (set! (.-entity-key rectangle) entity-key)
+    (set! (.-scene-key rectangle) scene-key)
     (map->MatterPhysics2DRectangle
-     {:body rectangle :x x :y y :w w :h h})))
+     {:body rectangle :x x :y y :w w :h h :pivot-x pivot-x :pivot-y pivot-y})))
 
 (defn- existing-rectangle [{:context/keys [scene-key state]}]
   (.add World (state/get-world scene-key) (:body state))

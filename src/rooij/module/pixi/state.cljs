5f8bb92c298c1a8c7828f34febeb6d32fec12e13
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-05-07 18:19:16 +0200

    Rename variables

--- src/rooij/module/pixi/state.cljs
+++ src/rooij/module/pixi/state.cljs
@@ -12,13 +12,11 @@
 
 (defonce sheets (atom nil))
 
-;; TODO Rename textures to spritesheet-textures
-(defonce textures (atom nil))
+(defonce spritesheet-textures (atom nil))
 
-;; TODO Rename textures-2 to textures
-(defonce textures-2 (atom {}))
+(defonce spritesheet-animations (atom nil))
 
-(defonce animations (atom nil))
+(defonce textures (atom {}))
 
 (defonce scale (atom 1))
 
@@ -45,21 +43,21 @@
 
 (defn add-spritesheet! [name sheet]
   (swap! sheets assoc name sheet)
-  (swap! textures assoc name (js-keys->clj-keys (.-textures sheet)))
-  (swap! animations assoc name (js-keys->clj-keys (.-animations (.-spritesheet sheet)))))
+  (swap! spritesheet-textures assoc name (js-keys->clj-keys (.-textures sheet)))
+  (swap! spritesheet-animations assoc name (js-keys->clj-keys (.-animations (.-spritesheet sheet)))))
 
 (defn add-texture! [name texture]
-  (swap! textures-2 assoc name texture))
+  (swap! textures assoc name texture))
 
 (defn spritesheet-animation-texture [spritesheet animation]
-  (or (get-in @animations [spritesheet animation])
+  (or (get-in @spritesheet-animations [spritesheet animation])
       (timbre/error ::spritesheet-animation-texture-not-found
                     {:spritesheet spritesheet :animation animation})))
 
 (defn spritesheet-static-texture [spritesheet texture]
-  (or (get-in @textures [spritesheet texture])
+  (or (get-in @spritesheet-textures [spritesheet texture])
       (timbre/error ::spritesheet-static-texture-not-found
                     {:spritesheet spritesheet :texture texture})))
 
 (defn texture [name]
-  (get @textures-2 name))
+  (get @textures name))
